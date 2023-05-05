7f9e076ee3ab931122083f9817b9f5ec7d567f30
Hadrian Chio <hadrian.chio@gmail.com>
2023-05-05 16:53:32 +0200

    try implementation i18next

--- App.tsx
+++ App.tsx
@@ -5,9 +5,12 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ApplicationProvider } from "@ui-kitten/components";
 import { hideAsync, preventAutoHideAsync } from "expo-splash-screen";
 import React, { useEffect, useMemo, useState } from "react";
+import { I18nextProvider } from "react-i18next";
 import { Text } from "react-native";
 import AuthContext from "./src/contexts/auth-context";
 import useUbuntuFont from "./src/hooks/use-fonts";
+import "./src/i18n/config";
+import i18n from "./src/i18n/config";
 import HomeScreen from "./src/screens/home-screen";
 import SessionScreen from "./src/screens/session-screen";
 
@@ -59,27 +62,29 @@ const App: React.FC = () => {
   }
 
   return (
-    <AuthContext.Provider value={authContextValue}>
-      <ApplicationProvider {...eva} theme={eva.light}>
-        <NavigationContainer>
-          <Stack.Navigator>
-            {userToken ? (
-              <Stack.Screen
-                name='Home'
-                component={HomeScreen}
-                options={{ headerShown: false }}
-              />
-            ) : (
-              <Stack.Screen
-                name='Login'
-                component={SessionScreen}
-                options={{ headerShown: false }}
-              />
-            )}
-          </Stack.Navigator>
-        </NavigationContainer>
-      </ApplicationProvider>
-    </AuthContext.Provider>
+    <I18nextProvider i18n={i18n}>
+      <AuthContext.Provider value={authContextValue}>
+        <ApplicationProvider {...eva} theme={eva.light}>
+          <NavigationContainer>
+            <Stack.Navigator>
+              {userToken ? (
+                <Stack.Screen
+                  name='Home'
+                  component={HomeScreen}
+                  options={{ headerShown: false }}
+                />
+              ) : (
+                <Stack.Screen
+                  name='Login'
+                  component={SessionScreen}
+                  options={{ headerShown: false }}
+                />
+              )}
+            </Stack.Navigator>
+          </NavigationContainer>
+        </ApplicationProvider>
+      </AuthContext.Provider>
+    </I18nextProvider>
   );
 };
 
