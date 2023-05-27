9d483b0a0d5c8afc60e718d8200b926381b5b461
Hadrian Chio <hadrian.chio@gmail.com>
2023-05-27 17:05:14 +0200

    adjusted type exports and added example of fetching user data

--- src/components/common/goals/goals-container.tsx
+++ src/components/common/goals/goals-container.tsx
@@ -1,35 +1,49 @@
-import React from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { View } from "react-native";
+import { API_BASE_URL } from "../../../utils/config/config";
+import { UserGoals } from "../../../utils/types/types";
 import GoalsCard from "./goals-card";
 
 interface GoalsContainerProps {}
 
 const GoalsContainer: React.FC<GoalsContainerProps> = () => {
-    const goals = [
-        {
-            // image: require(""),
-            title: "Abgabe",
-            description: "Abgabe vorbereiten",
-        },
-        {
-            // image: require(""),
-            title: "Lorem",
-            description: "Lorem Ipsum",
-        },
-    ];
+  const [userGoals, setUserGoals] = useState<UserGoals[]>([]);
 
-    return (
-        <View>
-            {goals.map((goal, index) => (
-                <GoalsCard
-                    key={index}
-                    // image={goal.image}
-                    title={goal.title}
-                    description={goal.description}
-                />
-            ))}
-        </View>
-    );
+  useEffect(() => {
+    async function getUserGoals() {
+      try {
+        const token = await AsyncStorage.getItem("access_token");
+        if (token) {
+          const response = await axios.get(`${API_BASE_URL}goals`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+
+          setUserGoals(response.data.data);
+        }
+      } catch (error) {
+        console.error("Failed to get user goals", error);
+      }
+    }
+
+    getUserGoals();
+  }, [userGoals]);
+
+  return (
+    <View>
+      {userGoals.map((goal, index) => (
+        <GoalsCard
+          key={index}
+          // image={goal.image}
+          title={goal.title}
+          description={goal.description}
+        />
+      ))}
+    </View>
+  );
 };
 
 export default GoalsContainer;
