894e800fd25e95a108f3bb54072f851a18ce520f
Hadrian Chio <hadrian.chio@gmail.com>
2023-05-31 12:43:33 +0200

    finalised style of no tools available

--- src/screens/home-screen.tsx
+++ src/screens/home-screen.tsx
@@ -1,5 +1,6 @@
 import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
 import { useNavigation } from "@react-navigation/native";
+import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import {
@@ -13,11 +14,13 @@ import RoutineTool from "../components/card/tools/routine-tool";
 import GoalsIcon from "../components/card/tools/tools-svg/goals-icon";
 import JournalIcon from "../components/card/tools/tools-svg/journal-icon";
 import NotesIcon from "../components/card/tools/tools-svg/notes-icon";
+import SelectIcon from "../components/card/tools/tools-svg/select-icon";
 import TimerIcon from "../components/card/tools/tools-svg/timer-icon";
 import TodoIcon from "../components/card/tools/tools-svg/todo-icon";
 import ScreenWrapper from "../components/common/screen-wrapper";
 import AppText from "../components/common/typography/app-text";
 import useUserMe from "../hooks/use-user-me";
+import AppColors from "../utils/constants/colors";
 import { StatusBarColor } from "../utils/types/enums";
 import { AuthenticatedStackParamList } from "../utils/types/types";
 
@@ -74,26 +77,44 @@ const HomeScreen: React.FC = () => {
           <AppText fontStyle='heading3' colorStyle='black64'>
             {t("my-day.your-tools")}
           </AppText>
-          <View style={styles.favouriteToolsContainer}>
-            {currentUser.userTools.map((tool) => {
-              return (
-                <RoutineTool
-                  key={tool.tool.titleKey}
-                  title={t(tool.tool.titleKey)}
-                  navigateTo={() => navigateToScreen(tool.tool.screenName)}
-                >
-                  {getIconComponent(tool.tool.screenName)}
-                </RoutineTool>
-              );
-            })}
-          </View>
-        </View>
-        <View style={styles.editToolsContainer}>
-          <TouchableOpacity>
-            <AppText fontStyle='body' colorStyle='black64'>
-              {t("general.edit")}
-            </AppText>
-          </TouchableOpacity>
+          {currentUser.userTools.length != 0 ? (
+            <>
+              <View style={styles.favouriteToolsContainer}>
+                {currentUser.userTools.map((tool) => {
+                  return (
+                    <RoutineTool
+                      key={tool.tool.titleKey}
+                      title={t(tool.tool.titleKey)}
+                      navigateTo={() => navigateToScreen(tool.tool.screenName)}
+                    >
+                      {getIconComponent(tool.tool.screenName)}
+                    </RoutineTool>
+                  );
+                })}
+              </View>
+              <View style={styles.editToolsContainer}>
+                <TouchableOpacity>
+                  <AppText fontStyle='body' colorStyle='black64'>
+                    {t("general.edit")}
+                  </AppText>
+                </TouchableOpacity>
+              </View>
+            </>
+          ) : (
+            <TouchableOpacity style={styles.noToolsContainer}>
+              <LinearGradient
+                colors={["#296879", "#6F99A5"]}
+                style={styles.linearGradient}
+              >
+                <SelectIcon style={styles.iconPlacement} />
+              </LinearGradient>
+              <View style={styles.textPlacement}>
+                <AppText fontStyle='body' colorStyle='black70'>
+                  {t("my-day.add-tools")}
+                </AppText>
+              </View>
+            </TouchableOpacity>
+          )}
         </View>
       </ScrollView>
     </ScreenWrapper>
@@ -122,5 +143,31 @@ const styles = StyleSheet.create({
   editToolsContainer: {
     marginTop: 60,
     alignItems: "center",
+  },
+  noToolsContainer: {
+    marginTop: 30,
+    width: "100%",
+    height: 100,
+    borderRadius: 13,
+    backgroundColor: AppColors.blue300,
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  linearGradient: {
+    display: "flex",
+    borderRadius: 10,
+    height: 72,
+    width: 72,
+  },
+  iconPlacement: {
+    position: "relative",
+    top: 5,
+  },
+  textPlacement: {
+    width: 200,
+    height: 55,
+    justifyContent: "center",
   },
 });
