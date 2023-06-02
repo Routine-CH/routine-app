5b76f4de0223079da869e7a36741676f980ef9c6
sarcan <sarmayca@protonmail.ch>
2023-06-02 15:54:51 +0200

    after rebase

--- src/screens/profile-screen.tsx
+++ src/screens/profile-screen.tsx
@@ -13,74 +13,73 @@ import useUserMe from "../hooks/use-user-me";
 import AppColors from "../utils/constants/colors";
 
 const ProfileScreen = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    const currentUser = useUserMe();
-    console.log(currentUser);
-    const defaultAvatar = "../assets/misc/stones.jpg";
+  const currentUser = useUserMe();
+  console.log(currentUser);
+  const defaultAvatar = "../assets/misc/stones.jpg";
 
-    return (
-        <ScrollView style={{ backgroundColor: AppColors.white }}>
-            <ScreenWrapper>
-                <View style={{ paddingHorizontal: 20 }}>
-                    <View style={styles.iconContainer}>
-                        <IconButton iconName="pencil" />
-                    </View>
-                    {currentUser ? (
-                        <View style={styles.userInformation}>
-                            <Image
-                                source={require(defaultAvatar)}
-                                style={styles.profilePicture}
-                            />
-                            <AppText
-                                fontStyle="bodyMedium"
-                                style={[styles.textColor, { marginBottom: 10 }]}
-                            >
-                                {t("profile.hi")}{" "}
-                                {currentUser.currentUser?.username} 😄
-                            </AppText>
-                            <AppText fontStyle="body" style={styles.textColor}>
-                                {t("profile.since")} Juni 2023{" "}
-                                {t("profile.here")}
-                            </AppText>
-                        </View>
-                    ) : (
-                        <AppText> {t("shared-auth.no-user-data")} </AppText>
-                    )}
-                    <AchievementCard />
-                    <BadgesView />
-                </View>
-                <Badge />
-                <View style={styles.wrapper}>
-                    <YearCard currentUser={currentUser} />
-                </View>
-                <WeekView />
-            </ScreenWrapper>
-        </ScrollView>
-    );
+  return (
+    <ScrollView style={{ backgroundColor: AppColors.white }}>
+      <ScreenWrapper>
+        <View style={{ paddingHorizontal: 20 }}>
+          <View style={styles.iconContainer}>
+            <IconButton iconName="pencil" />
+          </View>
+          {currentUser ? (
+            <View style={styles.userInformation}>
+              <Image
+                source={require(defaultAvatar)}
+                style={styles.profilePicture}
+              />
+              <AppText
+                fontStyle="bodyMedium"
+                style={[styles.textColor, { marginBottom: 10 }]}
+              >
+                {t("profile.hi")} {currentUser.currentUser?.username} 😄
+              </AppText>
+              <AppText fontStyle="body" style={styles.textColor}>
+                {t("profile.since")} {currentUser.currentUser?.createdAt}
+                {t("profile.here")}
+              </AppText>
+            </View>
+          ) : (
+            <AppText> {t("general.no-user-data")} </AppText>
+          )}
+          <AchievementCard />
+          <BadgesView />
+        </View>
+        <Badge />
+        <View style={styles.wrapper}>
+          <YearCard currentUser={currentUser} />
+        </View>
+        <WeekView />
+      </ScreenWrapper>
+    </ScrollView>
+  );
 };
 
 export default ProfileScreen;
 
 const styles = StyleSheet.create({
-    wrapper: {
-        paddingTop: 60,
-        paddingHorizontal: 20,
-    },
-    iconContainer: {
-        position: "absolute",
-        right: 20,
-    },
-    userInformation: {
-        alignItems: "center",
-    },
-    textColor: {
-        color: AppColors.black64,
-    },
-    profilePicture: {
-        height: 125,
-        width: 125,
-        borderRadius: 100,
-        marginBottom: 30,
-    },
+  wrapper: {
+    paddingTop: 60,
+    paddingHorizontal: 20,
+  },
+  iconContainer: {
+    position: "absolute",
+    right: 20,
+  },
+  userInformation: {
+    alignItems: "center",
+  },
+  textColor: {
+    color: AppColors.black64,
+  },
+  profilePicture: {
+    height: 125,
+    width: 125,
+    borderRadius: 100,
+    marginBottom: 30,
+  },
 });
