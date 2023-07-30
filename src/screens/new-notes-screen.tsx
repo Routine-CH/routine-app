faaccd415a7f3efeb92e610a24b26a9c22afdca0
Hadrian Chio <hadrian.chio@gmail.com>
2023-07-30 20:09:32 +0200

    fixed label input field

--- src/screens/new-notes-screen.tsx
+++ src/screens/new-notes-screen.tsx
@@ -1,7 +1,4 @@
-import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
-import { useNavigation } from "@react-navigation/native";
-import { useEffect, useState } from "react";
-import { Controller, useForm } from "react-hook-form";
+import { Controller } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import { StyleSheet, View } from "react-native";
 import IconButton from "../components/common/buttons/icon-button";
@@ -9,66 +6,14 @@ import SaveButton from "../components/common/buttons/save-button";
 import LabelInputField from "../components/common/input/label-input-field";
 import ScrollViewScreenWrapper from "../components/common/scroll-view-screen-wrapper";
 import RoutineToast from "../components/common/toast/routine-toast";
-import { showToast } from "../components/common/toast/show-toast";
-import { createNoteRequest } from "../data/note/create-request";
+import useNewNote from "../hooks/notes/use-new-note";
 import AppColors from "../utils/constants/colors";
 import AppFontStyle from "../utils/constants/font-style";
-import { StatusBarColor, ToastType } from "../utils/types/enums";
-import {
-  AuthenticatedStackParamList,
-  IFormNoteInputs,
-} from "../utils/types/types";
+import { StatusBarColor } from "../utils/types/enums";
 
 const NewNotesScreen = () => {
   const { t } = useTranslation();
-  const { control, handleSubmit } = useForm<IFormNoteInputs>();
-  const [errorMessage, setErrorMessage] = useState("");
-  const navigation =
-    useNavigation<BottomTabNavigationProp<AuthenticatedStackParamList>>();
-
-  const handleNewNote = async ({
-    title,
-    description,
-    images = [],
-  }: IFormNoteInputs) => {
-    const response = await createNoteRequest({
-      title,
-      description,
-      images: images.map((image) => ({
-        id: image.id,
-        imageUrl: image.imageUrl,
-      })),
-    });
-    if (typeof response === "string") {
-      setErrorMessage(response);
-      showToast(ToastType.error, response);
-      setErrorMessage("");
-    } else if (response && response.status === 201) {
-      showToast(ToastType.success, "Notiz gespeichert");
-      setTimeout(() => {
-        navigation.navigate("Notes");
-      }, 2000);
-    } else {
-      setErrorMessage("Something is wrong");
-      showToast(ToastType.error, errorMessage);
-      setErrorMessage("");
-    }
-  };
-
-  const onErrors = (errors: any) => {
-    if (errors.title) {
-      setErrorMessage(errors.title.message);
-    } else if (errors.description) {
-      setErrorMessage(errors.description.message);
-    }
-  };
-
-  useEffect(() => {
-    if (errorMessage) {
-      showToast(ToastType.error, errorMessage);
-      setErrorMessage("");
-    }
-  }, [errorMessage]);
+  const { control, handleSubmit, handleNewNote, onErrors } = useNewNote();
 
   return (
     <ScrollViewScreenWrapper
@@ -86,7 +31,6 @@ const NewNotesScreen = () => {
           render={({ field: { onChange, onBlur, value } }) => (
             <LabelInputField
               placeholder={t("journal.title")}
-              /* @ts-ignore: TODO: fix this */
               inputStyle={styles.labelInput}
               multiline
               onBlur={onBlur}
@@ -94,7 +38,7 @@ const NewNotesScreen = () => {
               value={value}
             />
           )}
-          name="title"
+          name='title'
           rules={{
             required: "Bitte gib deiner Notiz einen Titel",
             minLength: {
@@ -106,17 +50,16 @@ const NewNotesScreen = () => {
         <Controller
           control={control}
           render={({ field: { onChange, onBlur, value } }) => (
-            /* @ts-ignore: TODO: fix this */
             <LabelInputField
               placeholder={t("notes.note")}
-              style={styles.labelDescriptionInput}
+              inputStyle={styles.labelDescriptionInput}
               multiline={true}
               onBlur={onBlur}
               onChangeText={(value) => onChange(value)}
               value={value}
             />
           )}
-          name="description"
+          name='description'
           rules={{
             required: "Bitte gib deiner Notiz eine Beschreibung",
             minLength: {
@@ -128,10 +71,10 @@ const NewNotesScreen = () => {
       </View>
       <View style={styles.iconContainer}>
         <IconButton
-          iconName="camera"
+          iconName='camera'
           style={[styles.iconStyle, { marginRight: 15 }]}
         />
-        <IconButton iconName="images" style={styles.iconStyle} />
+        <IconButton iconName='images' style={styles.iconStyle} />
       </View>
       <RoutineToast />
     </ScrollViewScreenWrapper>
