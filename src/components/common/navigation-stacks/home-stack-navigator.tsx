815d79014ce50083302bef82d98c11625b342ed7
Hadrian Chio <hadrian.chio@gmail.com>
2023-07-31 11:52:01 +0200

    added camera blueprint

--- src/components/common/navigation-stacks/home-stack-navigator.tsx
+++ src/components/common/navigation-stacks/home-stack-navigator.tsx
@@ -13,6 +13,7 @@ import NoteViewScreen from "../../../screens/note-view-screen";
 import NotesScreen from "../../../screens/notes-screen";
 import TimerScreen from "../../../screens/timer-screen";
 import TodosScreen from "../../../screens/todos-screen";
+import { CameraView } from "../camera/camera-view";
 
 // declare stack navigator
 const HomeStack = createNativeStackNavigator();
@@ -20,25 +21,26 @@ const HomeStack = createNativeStackNavigator();
 const HomeStackNavigator: React.FC = () => {
   return (
     <HomeStack.Navigator
-      initialRouteName="UserHome"
+      initialRouteName='UserHome'
       screenOptions={{ headerShown: false }}
     >
-      <HomeStack.Screen name="UserHome" component={HomeScreen} />
-      <HomeStack.Screen name="Timer" component={TimerScreen} />
-      <HomeStack.Screen name="Journals" component={JournalsScreen} />
+      <HomeStack.Screen name='UserHome' component={HomeScreen} />
+      <HomeStack.Screen name='Timer' component={TimerScreen} />
+      <HomeStack.Screen name='CameraView' component={CameraView} />
+      <HomeStack.Screen name='Journals' component={JournalsScreen} />
       {/* @ts-ignore: TODO: fix this */}
-      <HomeStack.Screen name="JournalEdit" component={EditJournalScreen} />
-      <HomeStack.Screen name="JournalNew" component={NewJournalScreen} />
-      <HomeStack.Screen name="Todos" component={TodosScreen} />
-      <HomeStack.Screen name="TodosNew" component={NewTodosScreen} />
-      <HomeStack.Screen name="Notes" component={NotesScreen} />
-      <HomeStack.Screen name="NotesNew" component={NewNotesScreen} />
+      <HomeStack.Screen name='JournalEdit' component={EditJournalScreen} />
+      <HomeStack.Screen name='JournalNew' component={NewJournalScreen} />
+      <HomeStack.Screen name='Todos' component={TodosScreen} />
+      <HomeStack.Screen name='TodosNew' component={NewTodosScreen} />
+      <HomeStack.Screen name='Notes' component={NotesScreen} />
+      <HomeStack.Screen name='NotesNew' component={NewNotesScreen} />
       {/* @ts-ignore: TODO: fix this */}
-      <HomeStack.Screen name="NotesEdit" component={EditNotesScreen} />
+      <HomeStack.Screen name='NotesEdit' component={EditNotesScreen} />
       {/* @ts-ignore: TODO: fix this */}
-      <HomeStack.Screen name="NoteView" component={NoteViewScreen} />
-      <HomeStack.Screen name="Goals" component={GoalsScreen} />
-      <HomeStack.Screen name="EditTools" component={EditToolsScreen} />
+      <HomeStack.Screen name='NoteView' component={NoteViewScreen} />
+      <HomeStack.Screen name='Goals' component={GoalsScreen} />
+      <HomeStack.Screen name='EditTools' component={EditToolsScreen} />
     </HomeStack.Navigator>
   );
 };
