198bbd9bde64fadb548aed73aad2f951653f8803
Hadrian Chio <hadrian.chio@gmail.com>
2023-08-05 16:56:36 +0200

    disabled timer until further notice

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,22 +1,20 @@
 import FavGoalsIcon from "../components/card/tools/tools-svg/fav/fav-goals-icon";
 import FavJournalIcon from "../components/card/tools/tools-svg/fav/fav-journal-icon";
 import FavNotesIcon from "../components/card/tools/tools-svg/fav/fav-notes-icon";
-import FavTimerIcon from "../components/card/tools/tools-svg/fav/fav-timer-icon";
 import FavTodoIcon from "../components/card/tools/tools-svg/fav/fav-todo-icon";
 import GoalsIcon from "../components/card/tools/tools-svg/goals-icon";
 import JournalIcon from "../components/card/tools/tools-svg/journal-icon";
 import NotesIcon from "../components/card/tools/tools-svg/notes-icon";
-import TimerIcon from "../components/card/tools/tools-svg/timer-icon";
 import TodoIcon from "../components/card/tools/tools-svg/todo-icon";
 
 export const routineTools = [
-  {
-    id: "8fe84749-a5e4-4674-81ed-f255b923c386",
-    titleKey: "tool-cards.timer",
-    screenName: "Timer",
-    IconComponent: TimerIcon,
-    favIconComponent: FavTimerIcon,
-  },
+  // {
+  //   id: "8fe84749-a5e4-4674-81ed-f255b923c386",
+  //   titleKey: "tool-cards.timer",
+  //   screenName: "Timer",
+  //   IconComponent: TimerIcon,
+  //   favIconComponent: FavTimerIcon,
+  // },
   {
     id: "0a0799ce-eb61-4f0e-813b-a751da2e1940",
     titleKey: "tool-cards.journals",
